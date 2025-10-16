4918f99fd252900c059af02afd2a5e8f74fd1afc
dbkloh <devonhaarris@gmail.com>
2025-10-16 10:19:49 +0200

    Add TypeScript definitions for DevtoolsCard and Route

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export declare function DevtoolsCard(props?: Record<string, unknown>): React.ReactElement;
+export declare const DevTools: typeof DevtoolsCard;
+
+export declare namespace Route {
+  function GET(req: Request, ctx?: { params?: { slug?: string[] } }): Promise<Response>;
+  function POST(req: Request, ctx?: { params?: { slug?: string[] } }): Promise<Response>;
+}
