2d0788a4a52e843eeb91e5d79f40c7a6ac3ba58e
dbkloh <devonhaarris@gmail.com>
2025-10-16 10:30:22 +0200

    Remove unnecessary whitespace in index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
 import * as React from "react";
-
 export declare function DevtoolsCard(props?: Record<string, unknown>): React.ReactElement;
 export declare const DevTools: typeof DevtoolsCard;
-
 export declare namespace Route {
   function GET(req: Request, ctx?: { params?: { slug?: string[] } }): Promise<Response>;
   function POST(req: Request, ctx?: { params?: { slug?: string[] } }): Promise<Response>;
