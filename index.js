c4607cde98c18580ec396108aaaf85b1e2a2fa2a
dbkloh <devonhaarris@gmail.com>
2025-10-16 10:29:54 +0200

    Refactor root exports in index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// Root exports: a ready-to-use UI component + convenience re-exports
-export { DevtoolsCard as DevTools } from "./ui/card.js"; // named export { DevTools }
-export { DevtoolsCard } from "./ui/card.js";             // alternative name
+// Root exports
+export { DevtoolsCard as DevTools } from "./ui/card.js";   // { DevTools } dispo
+export { DevtoolsCard } from "./ui/card.js";               // alias
 export * as Route from "./adapters/next/catchall-route.js"; // { GET, POST }
-export { default as theme } from "./theme.css" assert { type: "css" }; // optional
 export default {};
